fb5c1844e925de1ef32c0aa3178cb2f907aa4822
Mobin <Mobinatashi2@gmail.com>
2022-05-15 23:20:43 +0430

    SPAMMER

--- poonisha_api.py
+++ poonisha_api.py
@@ -13,6 +13,17 @@ def poonisha_sms(phone: str):
                                   {"mobile": phone, "type": "2"}),
                               headers={"Content-Type": "application/json",
                                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"})
-
-    print("Poonisha_SMS_Done")
-    print("Poonisha_CALL_Done")
+    try:
+        status_sms = resp_sms.json()["status"]
+        status_call = resp_call.json()["status"]
+        if status_sms == "created":
+            print("Poonisha_SMS_Done")
+        else:
+            print("Poonisha_SMS_Failed")
+        if status_call == "exists":
+            print("Poonisha_CALL_Done")
+        else:
+            print("Poonisha_CALL_Failed")
+    except Exception as e:
+        print("Poonisha_SMS_Failed")
+        print("Poonisha_CALL_Failed")
