6d67c1240233bcb2f365a08910526777654116a8
Mobin <Mobinatashi2@gmail.com>
2022-05-15 23:28:47 +0430

    SPAMMER

--- taghcheh_api.py
+++ taghcheh_api.py
@@ -7,5 +7,14 @@ def taghcheh_sms(phone: str):
                              data=json.dumps(
                                  {"contact": phone, "forceOtp": False}),
                              headers={"Content-Type": "application/json"})
-
-    print("Taghcheh_Done")
+    try:
+        status_sms = resp_sms.json()["status"]
+        if status_sms == 0:
+            print("Taghcheh_Done")
+            return True
+        else:
+            print("Taghcheh_Failed")
+            return False
+    except Exception as e:
+        print("Taghcheh_Failed")
+        return False
