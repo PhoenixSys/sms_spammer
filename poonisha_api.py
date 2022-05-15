ee181dd14b950f05f0ca9185e097aecf8d3fd2c1
Mobin <Mobinatashi2@gmail.com>
2022-05-15 21:52:31 +0430

    SPAMMER

--- poonisha_api.py
+++ poonisha_api.py
@@ -10,6 +10,3 @@ def poonisha_sms(phone: str):
                                       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"})
     print("Poonisha_SMS_Done")
     print("Poonisha_CALL_Done")
-
-
-poonisha_sms("9142520208")
