439eb5d150161ff10a12db5b57bcd198d65275b4
Mobin <Mobinatashi2@gmail.com>
2022-05-15 23:23:07 +0430

    SPAMMER

--- sheypoor_api.py
+++ sheypoor_api.py
@@ -8,4 +8,13 @@ def sheypoor_sms(phone: str):
                                  {"username": phone}),
                              headers={"Content-Type": "application/json",
                                       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"})
-    print("Sheypoor_Done")
+    try:
+        status_sms = resp_sms.json()["success"]
+        if status_sms:
+            print("Sheypoor_Done")
+            return True
+        else:
+            print("Sheypoor_Failed")
+            return False
+    except Exception as e:
+        print("Sheypoor_Failed")
