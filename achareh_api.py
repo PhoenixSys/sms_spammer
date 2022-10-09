cd8592bbe727b1db38a519552afe208d0452522a
mrisis <rezaamin8889@gmail.com>
2022-10-09 23:14:37 +0330

    achareh api

---
+++ achareh_api.py
@@ -0,0 +1,23 @@
+import requests, json
+
+
+def acharh_sms(phone: str):
+    phone = "98" + phone
+    resp_sms = requests.post("https://api.achareh.co/v2/accounts/login/",
+                             data=json.dumps(
+                                 {"phone": phone}),
+                             headers={"Content-Type": "application/json"})
+    try:
+        status_sms = resp_sms.json()
+        if status_sms['is_new']:
+            print("Acahreh_Done")
+            return True
+        else:
+            print("Acahreh_Failed")
+            return False
+    except Exception as e:
+        print("Achareh_Failed")
+        return False
+
+if __name__ == "__main__":
+    acharh_sms('9019640184')
